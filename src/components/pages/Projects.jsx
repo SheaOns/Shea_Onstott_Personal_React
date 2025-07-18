2c180e6171aef8c1d6bc0caafabe569cd5b318da
Shea <sheaonstott@gmail.com>
2025-07-18 02:59:16 -0500

    test

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -4,9 +4,9 @@ import "../css/Projects.css"
 export const Projects = () => {
   return (
     <>
-      <div className="projects-container">
-        <h1>Coming Soon.</h1>
-      </div>
+      <div className="contact-container">
+        <h1>Coming <span className="soon">Soon.</span></h1>
+      </div>     
     </>
   );
 };
