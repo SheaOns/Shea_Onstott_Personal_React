4478c5cbb38b982391f4e8227360bda8d544fe7b
Shea <sheaonstott@gmail.com>
2025-07-18 17:54:32 -0500

    test

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -5,7 +5,7 @@ export const Contact = () => {
   return (
     <>
       <div className="contact-container">
-        <h1>Coming <span className="soon">Soon.</span></h1>
+        <h1>Comingkjfleskdjflksjf <span className="soon">Soon.</span></h1>
       </div>     
     </>
   )
